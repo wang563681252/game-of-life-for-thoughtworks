d2367a895895c63b302c5df25cde1bc13068f210
wang563681252 <563681252@qq.com>
2019-06-13 11:28:13 +0800

    重构目录结构

---
+++ src/lifeGame.h
@@ -0,0 +1,13 @@
+//
+// Created by dreamstart on 2019-06-13.
+//
+
+#ifndef CLION_CODE_LIFEGAME_H
+#define CLION_CODE_LIFEGAME_H
+
+#include <vector>
+using namespace std;
+
+void startLifeGame();
+
+#endif //CLION_CODE_LIFEGAME_H
